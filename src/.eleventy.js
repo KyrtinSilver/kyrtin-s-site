0c7cc83a8610c8cc668a154af1819b9306cc4545
ci-psy <31685851+ci-psy@users.noreply.github.com>
2024-12-18 09:17:31 -0800

    SVG DOM size reduction

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,4 +1,3 @@
 eleventyConfig.addPassthroughCopy("src/CNAME");
 eleventyConfig.addPassthroughCopy("src/robots.txt");
 eleventyConfig.addPassthroughCopy("src/assets");
-- eleventyConfig.addPassthroughCopy("src/_includes/assets"); 
