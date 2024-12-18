6236c2e6f5bc89505c7a917807cede7de1d807c1
ci-psy <31685851+ci-psy@users.noreply.github.com>
2024-12-18 08:41:34 -0800

    Selfhost font update
    Update to remove SPF and Google fonts renderblock request

---
+++ src/.eleventy.js
@@ -0,0 +1,4 @@
+eleventyConfig.addPassthroughCopy("src/CNAME");
+eleventyConfig.addPassthroughCopy("src/robots.txt");
+eleventyConfig.addPassthroughCopy("src/assets");
+- eleventyConfig.addPassthroughCopy("src/_includes/assets"); 
