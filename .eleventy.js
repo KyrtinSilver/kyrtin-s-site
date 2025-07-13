30f5745fb57c261eb1ff3d7bde8eda9ed9b56510
ci-psy <31685851+ci-psy@users.noreply.github.com>
2025-07-12 17:27:16 -0700

    rss feed update (test)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(rssPlugin);
+  
   eleventyConfig.addCollection("essays", function(collection) {
     return collection.getFilteredByGlob("src/essays/**/*.md")
       .filter(item => !item.inputPath.endsWith("index.md"))
